5d15dc0513b9d5afa797369e4fa6b74cacc04e02
Fran García <fgarciadevelop@gmail.com>
2026-07-08 22:25:51 +0200

    chore: change environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   name: 'development',
   production: false,
   CENSO_API_BASE_PATH: 'https://censo-api-dev.hogueras.es/emjf1/Censo-Hogueras/1.0.0',
-  SECRETARIA_API_BASE_PATH: 'https://secretaria-api-dev.hogueras.es/emjf1/Secretaria/1.0.0',
-  API_BASE_PATH: 'https://secretaria-api-dev.hogueras.es/emjf1/Secretaria/1.0.0',
+  SECRETARIA_API_BASE_PATH: 'https://ffsj-secretaria-api-dev.azurewebsites.net/emjf1/Secretaria/1.0.0',
+  API_BASE_PATH: 'https://ffsj-secretaria-api-dev.azurewebsites.net/emjf1/Secretaria/1.0.0',
   FILES_BASE_PATH: 'https://files-dev.secretaria.hogueras.es/uploads'
 };
