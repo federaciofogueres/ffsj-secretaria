a51baf74ab5ceb7887dc727b5371687802919fae
Fran García <fgarciadevelop@gmail.com>
2026-09-05 15:42:00 +0200

    fix(dev): route censo traffic through eastus contingency

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   name: 'development',
   production: false,
-  CENSO_API_BASE_PATH: 'https://ffsj-new-censo-api-dev.azurewebsites.net/emjf1/Censo-Hogueras/1.0.0',
+  CENSO_API_BASE_PATH: 'https://ffsj-new-censo-api-dev-eastus.azurewebsites.net/emjf1/Censo-Hogueras/1.0.0',
   SECRETARIA_API_BASE_PATH: 'https://ffsj-secretaria-api-dev.azurewebsites.net/emjf1/Secretaria/1.0.0',
   API_BASE_PATH: 'https://ffsj-secretaria-api-dev.azurewebsites.net/emjf1/Secretaria/1.0.0',
   FILES_BASE_PATH: 'https://files-dev.secretaria.hogueras.es/uploads'
